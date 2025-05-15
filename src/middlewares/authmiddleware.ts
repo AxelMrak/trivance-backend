2faa40c4eef2e701827c7983aa06e9ecb702980d
Nacho Sudo <nacho301011@gmail.com>
2025-05-15 14:41:36 -0300

    refactor authMiddleware for improved token validation and clarity

--- src/middlewares/authmiddleware.ts
+++ src/middlewares/authmiddleware.ts
@@ -1,26 +1,25 @@
-import { Response, NextFunction, Request } from "express";
+import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
+import { User } from "@/entities/User"; 
 
-const AuthMiddleware = (req: Request, res: Response, next: NextFunction) => {
+
+type JwtPayload = Pick<User, "id" | "name" | "email" | "company_id" | "role">;
+
+const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
   const token = req.cookies?.token;
 
   if (!token) {
     return res.status(403).json({ message: "No token provided" });
   }
 
-  const isTokenValid = jwt.verify(token, process.env.JWT_SECRET!);
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as JwtPayload;
 
-  if (!isTokenValid) {
-    return res.status(401).json({ message: "Unauthorized" });
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid token" });
   }
-
-  const decoded = jwt.decode(token);
-
-  if (!decoded) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
-
-  next();
 };
 
-export default AuthMiddleware;
+export default authMiddleware;
