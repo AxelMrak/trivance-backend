d9291f262a3f6c918df20918ef34d34703ae6d94
Nacho Sudo <nacho301011@gmail.com>
2025-05-15 15:20:31 -0300

    remove custom user type definition from Express Request

--- src/Types/express.d.ts
+++
@@ -1,9 +0,0 @@
-import { User } from "@/entities/User"; 
-
-declare global {
-  namespace Express {
-    interface Request {
-      user?: Pick<User, "id" | "company_id" | "name" | "email" | "role">;
-    }
-  }
-}
