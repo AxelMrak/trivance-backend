a316792231dc405bccd836d24a8c50eeb9fc74b5
Nacho Sudo <nacho301011@gmail.com>
2025-05-28 13:16:44 -0300

    feat(appointments): add AppointmentStatus enum and Appointment interface

---
+++ src/entities/appointment.ts
@@ -0,0 +1,16 @@
+export enum AppointmentStatus {
+    Pending = 2,
+    Confirmed = 1,
+    Cancelled = 0,
+  }
+  
+  export interface Appointment {
+    id: string;
+    serviceId: string;
+    userId: string;
+    enumStatus: AppointmentStatus;
+    startTime: Date;
+    endTime: Date;
+    createdAt: Date;
+    description?: string;
+  }
