d7faf8b992fdb7e32b7b540d8f9e823045202079
Axel Mrak <axelmrak@gmail.com>
2026-08-05 23:45:50 -0300

    add guards routes

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -3,6 +3,8 @@ import { Router } from "express";
 import { UserController } from "@controllers/UserController";
 import { UserService } from "@services/UserService";
 import { UserRepository } from "@repositories/UserRepository";
+import AuthMiddleware from "@middlewares/authmiddleware";
+import { attachUserRole, requireMinRole } from "@middlewares/roleMiddleware";
 
 const router = Router();
 
@@ -10,7 +12,7 @@ const userRepository = new UserRepository();
 const userService = new UserService(userRepository);
 const userController = new UserController(userService);
 
-router.get("/getAll", userController.getAll);
-router.get("/get/:id", userController.getById);
+router.get("/getAll", AuthMiddleware, attachUserRole(), requireMinRole(3), userController.getAll);
+router.get("/get/:id", AuthMiddleware, attachUserRole(), requireMinRole(3), userController.getById);
 
 export default router;
