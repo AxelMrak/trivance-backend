ecb82ff1844cf5833c95286a6e09caf0b56e2c43
Axel Mrak <axelmrak@gmail.com>
2026-08-05 23:46:05 -0300

    change service to use new types

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,14 +1,14 @@
 import { UserRepository } from "@repositories/UserRepository";
-import { User } from "@entities/User";
+import { PublicUserDTO } from "@entities/User";
 
 export class UserService {
   constructor(private repository: UserRepository) {}
 
-  async getUsers(): Promise<User[]> {
+  async getUsers(): Promise<PublicUserDTO[]> {
     return this.repository.findAll();
   }
 
-  async getUserByID(id: string): Promise<User | null> {
+  async getUserByID(id: string): Promise<PublicUserDTO | null> {
     return this.repository.findById(id);
   }
 }
